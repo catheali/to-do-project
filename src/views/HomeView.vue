a4ed9ea7eaef706560cc03dced8a5ac7f53f71b5
catheali <aliciacatherinealencar@gmail.com>
2023-02-24 14:27:52 -0300

    estudando conceitos basicos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h1>Homepage</h1>
+    <v-btn
+    class="pink white--text"
+    >clique me </v-btn>
+  
+    <v-btn  depressed dark color="pink">
+      clique aqui
+    </v-btn>
+    
+    <v-btn flat color="pink">clique</v-btn>
+ </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    
   }
-}
 </script>
