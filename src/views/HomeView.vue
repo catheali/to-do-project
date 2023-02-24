804865900d013c7b438ba2bbbc3e5d66f57e0f71
catheali <aliciacatherinealencar@gmail.com>
2023-02-24 14:50:03 -0300

    testes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
-    <h1>Homepage</h1>
-    <v-btn
-    class="pink white--text"
-    >clique me </v-btn>
-  
-    <v-btn  depressed dark color="pink">
-      clique aqui
+    <h1>Mitski</h1>
+    <v-btn text color="pink">toss your dirty shoes in my washing machine </v-btn>
+    <v-btn depressed class="pink white--text">
+      <v-icon left >mdi-heart</v-icon>
+      <span>heart</span>
+      <v-icon right >mdi-heart</v-icon>
     </v-btn>
-    
-    <v-btn flat color="pink">clique</v-btn>
+    <v-btn text color="pink">baby bring it up inside</v-btn>
+    <v-btn fab depressed small dark  color="purple">
+      <v-icon >mdi-music</v-icon>
+      
+    </v-btn>
  </div>
 </template>
 
