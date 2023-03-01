2d65fd2e568e842fc03d20c12fa398773315af78
catheali <aliciacatherinealencar@gmail.com>
2023-03-01 13:51:33 -0300

    validando form

--- src/components/PopNewProject.vue
+++ src/components/PopNewProject.vue
@@ -2,47 +2,86 @@
   <v-dialog 
   max-width="600px">
     <template 
-    v-slot:activator="{on}">
+    v-slot:activator="{ on }">
       <v-btn 
-      outlined
-      color="black"
+      outlined color="black" 
       v-on="on" 
-      dark>Add a new project</v-btn>
+      dark
+      >Add a new project</v-btn>
     </template>
     <v-card>
       <v-card-title>
         <h3>Add a new project</h3>
-       <v-card-text>
-        <v-form class="px-3">
-          <v-text-field class="" label="Title" v-model="title">
-          </v-text-field>
-          <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil">
-          </v-textarea>
-          <v-btn text class="success mx-0 mt-3" @click="submitForm">Add Project</v-btn>
-        </v-form>
-       </v-card-text>
-
+        <v-card-text>
+          <v-form 
+          class="px-3"
+          ref="form">
+            <v-text-field 
+            label="Title" 
+            v-model="title" 
+            :rules="rules"
+            prepend-icon="mdi-folder"
+            ></v-text-field>
+            <v-textarea 
+            label="Information" 
+            v-model="content" 
+            :rules="rules"
+            prepend-icon="mdi-pencil"
+            ></v-textarea>
+            <v-menu 
+            max-width="290">
+              <template 
+              v-slot:activator="{ on }">
+                <v-text-field 
+                :rules="rules"
+                :value="formattedDate" 
+                v-on="on" label="Due date" 
+                prepend-icon="mdi-calendar-range"
+                  slot="activator"
+                  ></v-text-field>
+              </template>
+              <v-date-picker 
+              v-model="due"
+              ></v-date-picker>
+            </v-menu>
+            <v-btn 
+            text 
+            class="success mx-0 mt-3" 
+            @click="submitForm"
+            >Add Project</v-btn>
+          </v-form>
+        </v-card-text>
       </v-card-title>
     </v-card>
   </v-dialog>
 </template>
 
 <script >
+import moment from 'moment'
 export default {
-  data(){
+  data() {
     return {
       title: '',
-      content: ''
+      content: '',
+      due: null,
+      rules: [
+        v => v.length >=3 || 'Minimum length is 3 characters',
+
+      ]
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? moment(this.due).format("Do MMMM YYYY") : "";
     }
   },
   methods: {
-    submitForm(){
-      console.log(this.content)
+    submitForm() {
+      if (this.$refs.form.validate()){
+        console.log(this.title, this.content, this.due)
+      }
     }
   }
 }
 </script>
-
-<style>
-
-</style>
+<style></style>
