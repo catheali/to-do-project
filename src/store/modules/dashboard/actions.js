1ebbedaacc306a67cb807182783064beedc775b2
catheali <aliciacatherinealencar@gmail.com>
2023-04-26 17:32:12 -0300

    Atualizando projetos

--- src/store/modules/dashboard/actions.js
+++ src/store/modules/dashboard/actions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import store from '@/store/index.js';
 
 export default {
-	async createNewProject({ commit }, payload) {
+	async createNewProject({ commit,dispatch }, payload) {
 		let user = store('auth').getters['auth/getUser'];
 		await axios.post(API + '/projects', {
 			title: payload.title,
@@ -11,7 +11,12 @@ export default {
 			status: payload.status,
 			due_time: payload.due,
 			user_id: user.id,
-		}).catch(function (error) {
+		})
+		.then(async function(){
+			await dispatch('getAllProjects');
+			await dispatch('getMyProjects', user.id);
+		})
+		.catch(function (error) {
 				commit('setError', {
 					valid: true,
 					message: error.response.data.error
@@ -42,6 +47,7 @@ export default {
 					valid: true,
 					message: 'Aconteceu algum problema ao buscar seus Projetos, verifique o erro e entre em contato: ' + error.data.message
 				})
+				
 				return error
 			})
 	},
@@ -66,6 +72,27 @@ export default {
 			.catch(function (error) {
 				return console.log(error.message);
 			})
-	}
+	},
+	async updateProject({ dispatch }, payload) {
+		let id = payload.id;
+		await axios.post(API + '/projects/'+ id, {
+			title: payload.title ? payload.title : null ,
+			content: payload.content ? payload.content : null,
+			status: payload.status ? payload.status : null,
+			due_time: payload.due ? payload.due : null,
+		}).then( async function(){
+			let user = store('auth').getters['auth/getUser'];
+			await dispatch('getMyProjects', user.id);
+			await dispatch('getAllProjects');
+		})
+		.catch(function (error) {
+					console.log('Deu ruim: '+ error.response.data.error)
+				
+			})
+	},
+	async 
+
+
+
 
 }
