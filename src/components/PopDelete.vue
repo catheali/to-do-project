0f5fba58fae4eaff06f6eff2ba3523e29014c984
catheali <aliciacatherinealencar@gmail.com>
2023-05-02 14:08:23 -0300

    avatar

--- src/components/PopDelete.vue
+++ src/components/PopDelete.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-dialog v-model="dialog" max-width="600px">
 		<template v-slot:activator="{ on }">
-			<v-btn outlined color="danger" v-on="on">Excluir</v-btn>
+			<v-btn outlined color="danger" small v-on="on">Excluir</v-btn>
 		</template>
 		<v-card >
 			<v-card-title>
